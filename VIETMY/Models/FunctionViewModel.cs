6b88f666d2fed1af0e3aa261de3f6c99f39c1f2e
pandacapricornus <duyvo@landsoft.com>
2017-07-28 09:24:25 +0700

    Ghi chú Commit

---
+++ VIETMY/Models/FunctionViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VIETMY.Models
+{
+    public class FunctionViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ VIETMY/Models/Messages.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VIETMY.Models
+{
+    public class Messages
+    {
+    }
+}
